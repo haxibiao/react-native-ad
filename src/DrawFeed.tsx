a87563e862c024c975dc8dbdc7eabec738450776
高璇 <gaoxuan@haxibiao.com>
2020-08-04 15:50:31 +0800

    统一Feed,DrawFeed事件名

--- src/DrawFeed.tsx
+++ src/DrawFeed.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { requireNativeComponent, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { requireNativeComponent, StyleSheet } from 'react-native';
 
-const DrawFeedComponent = requireNativeComponent("DrawFeedAd");
+const DrawFeedComponent = requireNativeComponent('DrawFeedAd');
 
 console.log('DrawFeedComponent', DrawFeedComponent);
 
@@ -9,42 +9,44 @@ console.log('DrawFeedComponent', DrawFeedComponent);
 // FIXME: 记得全部重新申请代码位，因为穿山甲马上要弃用旧代码位
 
 interface Props {
-	appid: string;
-	codeid: string;
-	onError?: Function;
-	onLoad?: Function;
-	onClick?: Function;
+    appid: string;
+    codeid: string;
+    visible?: boolean;
+    onAdError?: Function;
+    onAdShow?: Function;
+    onAdClick?: Function;
 }
 
 export const DrawFeedAd = (props: Props) => {
-	const { appid, codeid, onError, onLoad, onClick } = props;
-	const [visible, setVisible] = useState(true);
-	return (
-		visible && (
-			<DrawFeedComponent
-				provider={ "头条" }
-				appid={ appid }
-				codeid={ codeid }
-				style={ { ...styles.container } }
-				onAdError={ (e: any) => {
-					console.log("onError feed", e.nativeEvent);
-					setVisible(false);
-					onError && onError(e.nativeEvent);
-				} }
-				onAdClick={ onClick }
-				onAdShow={ (e: any) => {
-					console.log("onAdShow", e.nativeEvent);
-				} }
-			/>
-		)
-	);
+    const { appid, codeid, onAdError, onAdShow, onAdClick, visible = true } = props;
+    if (!visible) return null;
+    return (
+        <DrawFeedComponent
+            provider={'头条'}
+            appid={appid}
+            codeid={codeid}
+            style={{ ...styles.container }}
+            onAdError={(e: any) => {
+                console.log('onAdError DrawFeed', e.nativeEvent);
+                onAdError && onAdError(e.nativeEvent);
+            }}
+            onAdClick={(e: any) => {
+                console.log('onAdClick DrawFeed', e.nativeEvent);
+                onAdClick && onAdClick(e.nativeEvent);
+            }}
+            onAdShow={(e: any) => {
+                console.log('onAdShow DrawFeed', e.nativeEvent);
+                onAdShow && onAdShow(e.nativeEvent);
+            }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		width: "100%",
-	},
+    container: {
+        flex: 1,
+        width: '100%',
+    },
 });
 
 export default DrawFeedAd;
--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,64 +1,51 @@
-import React from "react";
-import { requireNativeComponent, StyleSheet } from "react-native";
+import React from 'react';
+import { requireNativeComponent, StyleSheet } from 'react-native';
 
-const FeedAdComponent = requireNativeComponent("FeedAd");
+const FeedAdComponent = requireNativeComponent('FeedAd');
 
 console.log('FeedAdComponent', FeedAdComponent);
 
-
 interface Props {
-	appid: string;
-	codeid: string;
-	adWidth: number;
-	visible: boolean;
-	visibleHandler: Function;
-	onAdLayout?: Function;
-	onAdError?: Function;
-	onAdClose?: Function;
-	onAdClick?: Function;
+    appid: string;
+    codeid: string;
+    adWidth?: number;
+    visible?: boolean;
+    onAdLayout?: Function;
+    onAdError?: Function;
+    onAdClose?: Function;
+    onAdClick?: Function;
 }
 
 const FeedAd = (props: Props) => {
-	const {
-		appid,
-		codeid,
-		adWidth = 150,
-		onAdLayout,
-		onAdError,
-		onAdClose,
-		onAdClick,
-	} = props;
-	// let [visible, setVisible] = useState(true);
-	// 状态交友父组件来控制，使得广告显示状态在父组件中可以实时监听
-	const { visible = true, visibleHandler } = props;
-	const [height, setHeight] = React.useState(0); // 默认高度
-	if (!visible) return null;
-	return (
-		<FeedAdComponent
-			appid={ appid ?? null }
-			codeid={ codeid }
-			adWidth={ adWidth }
-			style={ { width: adWidth, height } }
-			onAdError={ (e: any) => {
-				visibleHandler && visibleHandler(false);
-				onAdError && onAdError(e.nativeEvent);
-			} }
-			onAdClick={ (e: any) => {
-				onAdClick && onAdClick(e.nativeEvent);
-			} }
-			onAdClose={ (e: any) => {
-				visibleHandler && visibleHandler(false);
-				onAdClose && onAdClose(e.nativeEvent);
-			} }
-			onAdLayout={ (e: any) => {
-				if (e.nativeEvent.height) {
-					setHeight(e.nativeEvent.height);
-					onAdLayout && onAdLayout(e.nativeEvent);
-				}
-			} }
-		/>
-	);
+    const { appid, codeid, adWidth = 150, onAdLayout, onAdError, onAdClose, onAdClick } = props;
+    // let [visible, setVisible] = useState(true);
+    // 状态交友父组件来控制，使得广告显示状态在父组件中可以实时监听
+    const { visible = true, visibleHandler } = props;
+    const [height, setHeight] = React.useState(0); // 默认高度
+    if (!visible) return null;
+    return (
+        <FeedAdComponent
+            appid={appid ?? null}
+            codeid={codeid}
+            adWidth={adWidth}
+            style={{ width: adWidth, height }}
+            onAdError={(e: any) => {
+                onAdError && onAdError(e.nativeEvent);
+            }}
+            onAdClick={(e: any) => {
+                onAdClick && onAdClick(e.nativeEvent);
+            }}
+            onAdClose={(e: any) => {
+                onAdClose && onAdClose(e.nativeEvent);
+            }}
+            onAdLayout={(e: any) => {
+                if (e.nativeEvent.height) {
+                    setHeight(e.nativeEvent.height);
+                    onAdLayout && onAdLayout(e.nativeEvent);
+                }
+            }}
+        />
+    );
 };
-
 
 export default FeedAd;
