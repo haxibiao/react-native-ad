e27970e1740bf43eb3799f960d6b72e9d1b47a51
高璇 <gaoxuan@haxibiao.com>
2020-08-04 11:11:08 +0800

    暴露loadFeedAd方法预加载Feed缓存

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import { NativeModules } from "react-native";
+import { NativeModules } from 'react-native';
 
-import startSplash from "./Splash";
-import startFullVideo from "./FullScreenVideo";
-import startRewardVideo from "./RewardVideo";
-import DrawFeed from "./DrawFeed";
-import Feed from "./Feed";
+import startSplash from './Splash';
+import startFullVideo from './FullScreenVideo';
+import startRewardVideo from './RewardVideo';
+import DrawFeed from './DrawFeed';
+import Feed from './Feed';
 
 const { AdManager } = NativeModules;
 
 export const init = ({ appid }) => {
-	//FIXME: init 传入一些codeid可以提前加载广告，比如视频类
-	AdManager.init({ appid });
+    //FIXME: init 传入一些codeid可以提前加载广告，比如视频类
+    AdManager.init({ appid });
 };
 
 export const loadFeedAd = ({ appid }) => {
-	//提前加载信息流FeedAd
-	AdManager.loadFeedAd({ appid });
+    //提前加载信息流FeedAd
+    AdManager.loadFeedAd({ appid });
 };
 
 export default {
-	init,
-	startSplash,
-	startFullVideo,
-	startRewardVideo,
-	DrawFeed,
-	Feed,
+    init,
+    loadFeedAd,
+    startSplash,
+    startFullVideo,
+    startRewardVideo,
+    DrawFeed,
+    Feed,
 };
